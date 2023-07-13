6b2c6f64b0391fc75c02e7443a268658e02aad19
explos <explosblue@gmail.com>
2023-07-13 03:23:36 +0100

    Implement Cog with owner specific functionality

--- util/env.py
+++ util/env.py
@@ -9,6 +9,7 @@ load_dotenv(os.path.relpath("data.env"))
 
 DISCORD_BOT_TOKEN: Final[str] = os.getenv("DISCORD_BOT_TOKEN")
 DISCORD_TEST_GUILD: Final[str] = os.getenv("DISCORD_TEST_GUILD")
+DISCORD_OWNER_ID: Final[int] = int(os.getenv("DISCORD_OWNER_ID"))
 
 SUBSONIC_SERVER: Final[str] = os.getenv("SUBSONIC_SERVER")
 SUBSONIC_USER: Final[str] = os.getenv("SUBSONIC_USER")
